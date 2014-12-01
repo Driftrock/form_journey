33c7f09988dfbf378f33075b7b46472c278af868
Max <max.dupenois@forward.co.uk>
2014-12-01 11:53:44 +0000

    Single model concept for form journey

---
+++ lib/form_journey/uses_single_model.rb
@@ -0,0 +1,66 @@
+module FormJourney
+  module UsesSingleModel
+    extend ActiveSupport::Concern
+    included do
+      class_attribute :_model_class
+      class_attribute :_params_method
+    end
+
+    def edit
+      journey_params.clear
+      journey_params.set(:_model_object_id, value: params[:id])
+      redirect_to step_path(steps.first)
+    end
+
+    def editing?
+      model_object_id.present?
+    end
+
+    def model_object_id
+      journey_params.get(:_model_object_id)
+    end
+
+    private
+
+    def model_object
+      @model_object ||= begin
+        if editing?
+          self.class._model_class.find(model_object_id).tap do |obj|
+            obj.assign_attributes(model_params)
+          end
+        else
+          self.class._model_class.new(model_params)
+        end
+      end
+    end
+
+    def model_params
+      if self.class._params_method.respond_to?(:call)
+        self.class._params_method.call
+      else
+        self.send(self.class._params_method.to_sym)
+      end
+    end
+
+    module ClassMethods
+      def params_method(params_method)
+        self._params_method = params_method
+      end
+
+      def model_class(clasz)
+        clasz = clasz.is_a?(String) ? self.const_get(clasz) : clasz
+        self._model_class = clasz
+        hyphenated_class_name = clasz.to_s.gsub(/::/, '')
+          .gsub(/(?<=[^\b])([A-Z])/, '_\1')
+          .downcase
+        class_eval do
+          define_method(hyphenated_class_name.to_sym) do
+            model_object
+          end
+        end
+        self.send(:helper_method, hyphenated_class_name.to_sym)
+      end
+    end
+  end
+end
+
---
+++ spec/lib/form_journey/uses_single_model_spec.rb
@@ -0,0 +1,188 @@
+require 'spec_helper'
+
+RSpec.describe FormJourney::Controller do
+  class DummyModel
+  end
+
+  class DummySingleModelController < ApplicationController
+    include FormJourney::Controller
+    include FormJourney::UsesSingleModel
+    steps :step_one, :step_two, :step_three
+    params_method :obj_params
+    model_class DummyModel
+  end
+
+  let(:obj_params) { {} }
+
+  subject do
+    DummySingleModelController.new.tap do |cont|
+      allow(cont).to receive(:obj_params).and_return(obj_params)
+    end
+  end
+
+  let(:params) { {} }
+
+  let(:session) { {} }
+
+  let(:journey_params) do
+    FormJourney::Parameters.new(params, session)
+  end
+  
+  before do
+    allow(subject).to receive(:params).and_return(params)
+    allow(subject).to receive(:journey_params).and_return(journey_params)
+  end
+
+  describe '::params_method' do
+    context 'with a callable' do
+      let(:params_callable) { double(:callable) }
+      before do
+        subject.class.send(:params_method, params_callable)
+      end
+
+      describe '#model_params' do
+        it 'should call it' do
+          expect(params_callable).to receive(:call)
+          subject.send(:model_params)
+        end
+      end
+    end
+    
+    context 'with a string/sym' do
+      let(:params_method) { 'rargh' }
+      before do
+        subject.class.send(:params_method, params_method)
+      end
+
+      describe '#model_params' do
+        it 'should send it to self' do
+          expect(subject).to receive(params_method.to_sym)
+          subject.send(:model_params)
+        end
+      end
+    end
+  end
+
+  describe '::model_class' do
+    it 'should deal with a string' do
+      subject.class.send(:model_class, 'DummyModel')
+      expect(subject.class._model_class).to eq DummyModel
+    end
+
+    it 'should set the model class for the controller' do
+      expect(subject.class._model_class).to eq DummyModel
+    end
+
+    it 'should have assigned a method to to get the model object for the given object name' do
+      expect(subject).to respond_to(:dummy_model)
+    end
+
+    it 'should return the model object when the model name is called' do
+      expect(subject).to receive(:model_object)
+      subject.dummy_model
+    end
+  end
+
+  describe '#edit' do
+    let(:params) { { id: 132 } }
+    before do
+      allow(subject).to receive(:redirect_to)
+      allow(subject).to receive(:step_path)
+    end
+
+    it 'should clear the journey params' do
+      expect(journey_params).to receive(:clear)
+      subject.edit
+    end
+
+    it 'should assign the model object id' do
+      expect do
+        subject.edit
+      end.to change { journey_params.get(:_model_object_id) }
+        .from(nil).to params[:id]
+    end
+
+    it 'should redirect to the first step' do
+      redirect_location = 'blah'
+      expect(subject).to receive(:step_path).with(subject.steps.first)
+        .and_return(redirect_location)
+      expect(subject).to receive(:redirect_to).with(redirect_location)
+      subject.edit
+    end
+  end
+
+  describe '#model_object (via model name)' do
+    before do
+      subject.class.params_method :obj_params
+      subject.class.model_class DummyModel
+    end
+    let(:model_instance) do
+      DummyModel.new.tap do |model|
+        allow(model).to receive(:id).and_return(123213)
+        allow(model).to receive(:assign_attributes)
+      end
+    end
+
+    context 'when new' do
+      before do
+        allow(DummyModel).to receive(:new).and_return(model_instance)
+        journey_params.set(:_model_object_id, value: nil)
+      end
+
+      it 'should create a new instance of the model with object params' do
+        expect(DummyModel).to receive(:new).with(subject.obj_params)
+        subject.dummy_model
+      end
+
+      it 'should return the model instance' do
+        expect(subject.dummy_model).to be model_instance
+      end
+    end
+
+    context 'when editing' do
+      before do
+        journey_params.set(:_model_object_id, value: model_instance.id)
+        allow(DummyModel).to receive(:find).and_return(model_instance)
+      end
+
+      it 'should find the model instance' do
+        expect(DummyModel).to receive(:find).with(model_instance.id)
+          .and_return(model_instance)
+        subject.dummy_model
+      end
+
+      it 'should assign attributes to the model instance' do
+        expect(model_instance).to receive(:assign_attributes)
+          .with(subject.obj_params)
+        subject.dummy_model
+      end
+
+      it 'should return the model instance' do
+        expect(subject.dummy_model).to be model_instance
+      end
+    end
+  end
+
+  describe '#editing?' do
+    context 'when there is not a model object id' do
+      before do
+        journey_params.set(:_model_object_id, value: nil)
+      end
+
+      it 'should return false' do
+        expect(subject.editing?).to be false
+      end
+    end
+
+    context 'when there is a model object id' do
+      before do
+        journey_params.set(:_model_object_id, value: 3424)
+      end
+
+      it 'should return true' do
+        expect(subject.editing?).to be true
+      end
+    end
+  end
+
+end
