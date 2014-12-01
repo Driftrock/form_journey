ea64843adf86a3b84a47e5dabe29c1fe53571eb3
Max <max.dupenois@forward.co.uk>
2014-12-01 11:51:00 +0000

    Added routes and block concept to routes

--- lib/form_journey/routes.rb
+++ lib/form_journey/routes.rb
@@ -3,8 +3,12 @@ require 'action_dispatch'
 module ActionDispatch::Routing
   class Mapper
     def mount_journey(path, controller)
-      get "#{path}", controller: controller, action: :default_step
-      match "#{path}/:action", controller: controller, action: /[a-z0-9_]+/, via: [:get, :post], as: "#{path}_step"
+      resource "#{path}", only: [] do
+        get "/", controller: controller, action: :default_step
+        get "/:id/edit", controller: controller, action: 'edit', as: 'edit'
+        yield if block_given?
+        match "/:action", controller: controller, action: /[a-z0-9_]+/, via: [:get, :post, :patch, :delete], as: 'step'
+      end
     end
   end
 end
